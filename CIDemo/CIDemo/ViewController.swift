4925e74a37e0e8742eedfe4999169a30b5b67938
kaiye.lin <kaiye.lin@live.com>
2016-12-21 11:52:48 +0800

    init project

---
+++ CIDemo/CIDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIDemo
+//
+//  Created by kaiye.lin on 16/12/21.
+//  Copyright © 2016年 kaiye.lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
